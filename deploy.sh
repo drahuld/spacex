4a895c500d18925b511fec330582d1ec5362f08a
Rahul Deshmukh <deshmukh@artech-is.com>
2021-02-23 09:45:47 +0100

    deploy

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# abort on errors
+set -e
+# build
+npm run build
+# navigate into the build output directory
+cd dist
+cp index.html 404.html
+
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:drahuld/spacex.git main:gh-pages
+cd -
